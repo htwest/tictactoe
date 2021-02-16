6f5b537b534e30ced7cf58b420e0b21e4add5a25
Hayden West <haydenw7@gmail.com>
2021-02-15 18:17:01 -0700

    finished building board

---
+++ app.js
@@ -0,0 +1,19 @@
+// Initial Game State
+
+let currentPlayer = "X"
+let currentGame = ['','','','','','','','','']
+
+// Game Functionality
+
+function onCellClick = {
+
+}
+
+
+
+
+// Win or Draw Validation
+
+
+
+// Restart Game
